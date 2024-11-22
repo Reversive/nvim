daa8537fed590c0cf99aec111bdb7dac0b6e3d08
Reversive <matiaspavan@gmail.com>
2024-11-22 19:54:50 -0300

    feat: custom actions for telescope

---
+++ lua/reversive/lazy/telescope.lua
@@ -0,0 +1,34 @@
+return {
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim"
+        },
+        config = function()
+            local keymap = vim.keymap
+            local builtin = require('telescope.builtin')
+            local actions = require('telescope.actions')
+
+            require('telescope').setup({
+                defaults = {
+                    mappings = {
+                        i = {
+                            ["<C-j>"] = actions.move_selection_next,
+                            ["<C-k>"] = actions.move_selection_previous,
+                        },
+                        n = {
+                            ["<C-j>"] = actions.move_selection_next,
+                            ["<C-k>"] = actions.move_selection_previous,
+                        },
+                    },
+                },
+            })
+
+            keymap.set('n', '<leader>pf', builtin.find_files, {})
+            keymap.set('n', '<C-p>', builtin.git_files, {})
+            keymap.set('n', '<leader>ps', function()
+                builtin.grep_string({ search = vim.fn.input("Grep > ") })
+            end) 
+        end,
+    },
+}
