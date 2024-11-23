aa3be7b4a4b44f200f1a22bd38754bcd968d8233
Reversive <matiaspavan@gmail.com>
2024-11-22 21:37:55 -0300

    feat: toggleterm plugin

--- lua/reversive/keymaps.lua
+++ lua/reversive/keymaps.lua
@@ -1,4 +1,4 @@
-keymap = vim.keymap
+local keymap = vim.keymap
 
 vim.g.mapleader = " "
 
@@ -9,3 +9,7 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set({"n", "v"}, "<leader>d", "\"_d")
+vim.keymap.set("i", [[<C-\>]], '<cmd>exe v:count1 . "ToggleTerm"<CR>')
+vim.keymap.set("n", [[<C-\>]], '<cmd>exe v:count1 . "ToggleTerm"<CR>')
+vim.keymap.set("t", '<esc>', [[<C-\><C-n>]])
+vim.keymap.set("t", '<C-k>', [[<C-\><C-n><C-W>k]])
